e1480ce0e37ff35a7a73612ced57a604648ee5e3
*daymare* <87831835+rookieCookies@users.noreply.github.com>
2023-08-31 18:05:21 +0300

    hash map debug

--- src/hash/hash_map.rs
+++ src/hash/hash_map.rs
@@ -263,6 +263,14 @@ where
 }
 
 
+impl<K, V, S: HashFnSeed<K, Hash=u32>, A: Alloc> core::fmt::Debug for HashMap<K, V, S, A> 
+where K: core::fmt::Debug + Eq, V: core::fmt::Debug + Eq {
+    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+        f.debug_map().entries(self.iter()).finish()
+    }
+}
+
+
 #[cfg(test)]
 mod tests {
     use super::*;
